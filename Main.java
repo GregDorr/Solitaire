7d7183651ae132f4c7b63dd32292d4a5450ec4b9
Greg Dorr <dorrg@augsburg.edu>
2016-11-16 12:00:07 -0600

    Started working on way to change scene

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
-package Solitare;
+package Solitaire;
 
 import javafx.application.Application;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,13 +15,27 @@ import java.util.logging.Logger;
  */
 public class Main extends Application {
 
+    private Stage stage;
+    private BorderPane p1;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         try {
-            MainScene scene = new MainScene(new BorderPane());
-            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            //setting the top menu to the default border pane
+            p1 = new BorderPane();
+            TopMenu m1 = new TopMenu(this);
+            p1.setTop(m1);
+
+            //setting the stage
+            stage = primaryStage;
+
+            //adding it to the main scene
+            MainScene scene = new MainScene(p1);
             primaryStage.setTitle("Solitaire");
             primaryStage.setScene(scene);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            //setting the cursor
+
             primaryStage.show();
 
         } catch (Exception ex){
@@ -28,6 +43,13 @@ public class Main extends Application {
         }
     }
 
+    //public void setStage(MainScene scene){stage.setScene(scene);}
+    //public BorderPane getBorderPane(){return this.p1;}
+
+    /**
+     * MAIN
+     * @param args
+     */
     public static void main(String[] args) {
         launch(args);
     }
