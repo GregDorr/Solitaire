90a548a2499793a40784a10196500c05c968b926
Greg <Gregory.w.dorr@gmail.com>
2016-11-29 14:13:42 -0600

    Made variables global . Added setter and getter methods.

--- src/Solitaire/Main.java
+++ src/Solitaire/Main.java
@@ -1,13 +1,13 @@
 package Solitaire;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 
 /**
  * @author Greg
@@ -17,61 +17,60 @@ import java.util.logging.Logger;
  */
 public class Main extends Application {
 
-
+    private MainScene scene;
     private Stage stage;
-    private Stage newStage;
     private BorderPane p1;
     private int drawNum;
+    private FinalAnimation win;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         try {
-            Controller c1 = new Controller();
-
             drawNum = 1;
-
             //setting the top menu to the default border pane
             p1 = new BorderPane();
             TopMenu m1 = new TopMenu(this);
             p1.setTop(m1);
 
-            //setting the stage
+            BorderPane p2 = new BorderPane();
+            TopMenu m2 = new TopMenu(this);
+            p1.setTop(m2);
+
             stage = primaryStage;
 
+            //setting the stage
             //adding it to the main scene
-            MainScene scene = new MainScene(p1,3);
-
-            primaryStage.setTitle("Solitaire");
-            primaryStage.setScene(scene);
+            scene = new MainScene(p1,1,this);
+            stage.setTitle("Solitaire");
+            stage.setScene(scene);
             scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             //setting the cursor
+            stage.show();
 
-            primaryStage.show();
+            //14 solves whole thing
+          // scene.finish(14,false);
 
         } catch (Exception ex){
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public void setDrawNum(int num){
-        drawNum = num;
+    public void setDrawNum(int num){drawNum = num;}
+    public void TurnOnWinAnimation(){
+        stage.setScene(win);
     }
-
-    public void setStage(MainScene scene){stage.setScene(scene);}
-    public BorderPane getBorderPane(){return this.p1;}
-
-
-    //action listener for the settings
-    public void settings(final ActionEvent e){
-
+    public MainScene getScene(){
+        return scene;
     }
+    public void setScene(MainScene newScene){this.scene = newScene;}
+
+    public void setStage(Scene scene){stage.setScene(scene);}
 
     /**
      * MAIN
      * @param args
      */
     public static void main(String[] args) {
-
         launch(args);
     }
 }
